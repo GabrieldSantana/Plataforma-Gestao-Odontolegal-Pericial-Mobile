9c2baa546008eefbd9090809e53ad040bc2c8d76
Gabriel Meikii <gabrielmaeda01@gmail.com>
2025-06-08 15:14:27 -0300

    feat: Adicionar tela de cadastrar vítima após clicar no botão adicionar vítima

---
+++ src/app/(auth)/(tabs)/(casos)/AdicionarVitima.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+export default function AdicionarVitima(){
+    return (
+        <View>
+            <Text>Adicionar vitima tela, modifique a tela aqui Ket</Text>
+            <Text>Arquivo (casos)/AdicionarVitima.tsx</Text>
+        </View>
+    )
+}
